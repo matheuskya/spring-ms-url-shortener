9bdf24f42ed213c15371539fcdf094be9e7afa50
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2025-07-25 18:42:15 -0300

    dataloader e config corrigidos

--- user-service/src/main/java/com/matheuskya/user_service/model/DataLoader.java
+++ user-service/src/main/java/com/matheuskya/user_service/model/DataLoader.java
@@ -1,31 +1,34 @@
 package com.matheuskya.user_service.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Configuration
 public class DataLoader {
     private static final Logger log = LoggerFactory.getLogger(DataLoader.class);
 
     @Bean
-    CommandLineRunner initDatabase(UserRepository userRepository){
+    CommandLineRunner initDatabase(UserRepository userRepository) {
         return args -> {
-            User user1 = new User();
-            user1.setName("matheus");
-            user1.setPassword("123");
-            User user2 = new User();
-            user2.setName("lucas");
-            user2.setPassword("123");
+            if (userRepository.findAll().isEmpty()) {
+                User user1 = new User();
+                user1.setName("matheus");
+                user1.setPassword("123");
+                User user2 = new User();
+                user2.setName("lucas");
+                user2.setPassword("123");
 
-            userRepository.save(user1);
-            userRepository.save(user2);
+                userRepository.save(user1);
+                userRepository.save(user2);
 
-          log.info("preloading" + user1);
-          log.info("preloading" + user2);
+                log.info("preloading" + user1);
+                log.info("preloading" + user2);
+            } else {
+                log.info("Database already contains data");
+            }
         };
     }
 }
