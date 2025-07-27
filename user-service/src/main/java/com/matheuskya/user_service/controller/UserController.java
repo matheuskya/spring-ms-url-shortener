4d2d4fdb4e07aaf93310f93f6d2645304c581380
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2025-07-27 18:17:01 -0300

    controller alterado

--- user-service/src/main/java/com/matheuskya/user_service/controller/UserController.java
+++ user-service/src/main/java/com/matheuskya/user_service/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.matheuskya.user_service.controller;
 
+import com.matheuskya.user_service.dto.UserResponseDto;
 import com.matheuskya.user_service.model.User;
-import com.matheuskya.user_service.model.UserRepository;
+import com.matheuskya.user_service.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,14 +12,21 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository user){
-        this.userRepository = user;
+    public UserController(UserService userService) {
+        this.userService = userService;
+
     }
 
     @GetMapping("/user")
-    public List<User> getAll(){
-        return userRepository.findAll();
+    public List<User> getAll() {
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/user/{id}")
+    public UserResponseDto getById(@PathVariable String id) {
+        UserResponseDto response = userService.findUserById(id);
+        return response;
     }
 }
